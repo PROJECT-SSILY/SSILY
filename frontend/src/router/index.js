547c6acc7de46c557c9163b9e6626b3cd37c28aa
sooyeonlee127 <sooyeonlee127@gmail.com>
2023-01-20 15:41:29 +0900

    add : 로그인

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginPage from '../views/Accounts/LoginPage.vue'
+
 
 const routes = [
   {
@@ -14,7 +16,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginPage
+  },
 ]
 
 const router = createRouter({
---
+++ frontend/src/views/Accounts/LoginPage.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-form
+  ref="form"
+  v-model="valid"
+  lazy-validation
+  >
+    <v-text-field
+      v-model="email"
+      :counter="10"
+      :rules="emailRules"
+      label="이메일(아이디)"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="password"
+      :rules="passwordRules"
+      label="비밀번호"
+      required
+    ></v-text-field>
+
+    <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="validate"
+    >
+      로그인
+    </v-btn>
+
+    <v-btn
+      color="error"
+      class="mr-4"
+      @click="reset"
+    >
+      회원가입
+    </v-btn>
+
+    <v-btn
+      color="warning"
+      @click="resetValidation"
+    >
+      비밀번호 찾기
+    </v-btn>
+  </v-form>
+
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  setup() {
+    const email = '';
+    const password = '';
+    return {
+      email,
+      password
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
