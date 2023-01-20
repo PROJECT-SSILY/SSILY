05522021391407fa5f315a84843e54bf3f1cc683
ss0takk <ss0takk@gmail.com>
2023-01-20 15:24:13 +0900

    add : 도메인 설계

---
+++ backend/ssily/src/main/java/com/appleparty/ssily/domain/member/Member.java
@@ -0,0 +1,37 @@
+package com.appleparty.ssily.domain.member;
+
+import com.appleparty.ssily.domain.BaseEntity;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Member extends BaseEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String nickname;
+
+    private int level;
+
+    private long exp;
+}
