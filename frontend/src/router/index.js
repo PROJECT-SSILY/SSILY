62342acd4c9e513db19de997af3771d32eb2e73a
cherrytonic <fairy3530@naver.com>
2023-01-20 15:48:35 +0900

    add : 스타팅

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,20 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import StartingPage from '../views/StartingPage/StartingPage.vue'
+import LoginPage from '../views/Account/LoginPage.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Starting',
+    component: StartingPage
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/login',
+    name: 'Login',
+    component: LoginPage
+  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = createRouter({
---
+++ frontend/src/views/StartingPage/StartingPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <v-btn @click="toLogin">Start</v-btn>
+    </div>
+  </template>
+  
+<script>
+
+export default {
+name: 'StartingPage',
+methods: {
+    toLogin() {
+        this.$router.push({name: 'Login'})
+    }
+}
+
+}
+</script>
