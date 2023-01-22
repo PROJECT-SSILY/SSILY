a1a86b28f9fb0389ea07b2304eead5849e7f1742
ss0takk <ss0takk@gmail.com>
2023-01-22 14:36:43 +0900

    add : Spring Security 설정 파일 추가

---
+++ backend/ssily/src/main/java/com/appleparty/ssily/common/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.appleparty.ssily.common.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll();
+    }
+}
