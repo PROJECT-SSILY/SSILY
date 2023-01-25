392f76fe9063d5e4c89bce266dce159d1231eb17
Daniel LEE <dmsgur7112@gmail.com>
2023-01-25 10:10:27 +0900

    add : 마이페이지

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MyPage from '../views/MyPage.vue'
 
 const routes = [
   {
@@ -8,13 +9,11 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/mypage',
+    name: 'mypage',
+    component: MyPage
+  },
+
 ]
 
 const router = createRouter({
---
+++ frontend/src/views/MyPage.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="header">
+        <h1>마이 페이지</h1>
+    </div>
+    <div class="main">
+        <div class="section1">
+            <div class="inner_section1_left">
+                <img src="" alt="">
+            </div>
+            <div class="inner_section1_right">
+                <h2>{{ nickname }}님</h2>
+                <v-btn>회원정보 변경</v-btn>
+                <p>승률 : {{ winrate }}%</p>
+            </div>
+
+        </div>
+        <div class="section2">
+            <h2>나의 경험치 <span>{{ exp }} exp</span></h2>
+
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref, reactive } from 'vue'
+export default {
+    setup() {
+        const exp = ref(2723)
+        const nickname = ref('홍길동')
+        const history = reactive({ plays: 11, wins: 3, throws: 2 })
+        return {
+            exp,
+            nickname,
+            history,
+        }
+    },
+    computed: {
+        winrate() {
+            return this.history.wins/this.history.plays*100
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
