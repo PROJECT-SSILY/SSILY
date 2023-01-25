cb627cbd3a9d62eb9875b6b189f7ec0f414dc4fb
cherrytonic <fairy3530@naver.com>
2023-01-25 17:38:25 +0900

    add:컴포넌트 추가

--- frontend/src/views/MainPage/Components/RoomList.vue
+++ frontend/src/views/MainPage/Components/RoomList.vue
@@ -1,6 +1,48 @@
 <template>
     <div>
-      <v-btn>RoomList</v-btn>
+      <h3>
+        RoomList
+      </h3>
+      <v-card
+    class="mx-auto"
+    max-width="500"
+  >
+    <v-toolbar
+      color="deep-purple accent-4"
+      dark
+    >
+      <v-toolbar-title>New Chat</v-toolbar-title>
+      <v-switch
+      v-model="ex11"
+      label=""
+      color="orange darken-3"
+      value="orange darken-3"
+      hide-details
+    ></v-switch>
+
+    </v-toolbar>
+
+    <v-list subheader>
+
+      <v-list-item
+        v-for="chat in recent"
+        :key="chat.title"
+      >
+      
+      <v-list-item-avatar>
+      </v-list-item-avatar>
+      
+      <v-list-item-content>
+        <v-list-item-title v-text="chat.title"></v-list-item-title>
+      </v-list-item-content>
+      
+      <v-list-item-icon>
+      </v-list-item-icon>
+      <v-divider></v-divider>
+      </v-list-item>
+    </v-list>
+
+  </v-card>      
     </div>
   </template>
   
@@ -8,6 +50,27 @@
 
 export default {
 name: 'RoomList',
-
+data: () => ({
+      recent: [
+        {
+          active: true,
+          title: '들어오시죠',
+        },
+        {
+          active: true,
+          title: 'I 3D U',
+        },
+        {
+          active: true,
+          title: 'Draw me if you catch',
+        },
+        {
+          active: true,
+          title: '재밌다',
+        },
+      ],
+      ex11: ['red', 'indigo', 'orange', 'primary', 'secondary', 'success', 'info', 'warning', 'error', 'red darken-3', 'indigo darken-3', 'orange darken-3'],
+      
+    }),
 }
 </script>
