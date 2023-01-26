6a31a8acfa988cf162a98bd8a8459e543f6ef52e
sooyeonlee127 <sooyeonlee127@gmail.com>
2023-01-26 14:51:55 +0900

    refactor : setup함수로 변경

--- frontend/src/views/Accounts/LoginPage.vue
+++ frontend/src/views/Accounts/LoginPage.vue
@@ -1,24 +1,24 @@
 <template>
   <v-form
+  v-model="state.valid"
   ref="loginForm"
   lazy-validation
   >
     <v-text-field
-      v-model="email"
-      :rules="emailRules"
+      v-model="state.form.email"
       type="email"
+      :rules="emailRules"
       label="이메일(아이디)"
     ></v-text-field>
 
     <v-text-field
-      v-model="password"
+      v-model="state.form.password"
       type="password"
       label="비밀번호"
     ></v-text-field>
 
     <v-btn
       class="mr-4"
-      color="success"
       @click="clickLogIn"
     >
       로그인
@@ -26,7 +26,6 @@
 
     <v-btn
       class="mr-4"
-      color="success"
       @click="clickSignUp"
     >
       회원가입
@@ -34,39 +33,48 @@
 
     <v-btn
       class="mr-4"
-      color="success"
       @click="clickFindPw"
     >
       비밀번호 찾기
     </v-btn>
   </v-form>
-
 </template>
 
 <script>
+import { reactive, ref } from '@vue/reactivity'
+import { useRouter } from 'vue-router'
 export default {
   name: 'LoginPage',
+  setup() {
+    const loginForm = ref(null)
+    const state = reactive({
+      form: {
+        email: '',
+        password: '',
+      },
+      valid: true,
+    })
+    const router = useRouter()
+    function clickSignUp() {
+      router.push({
+        name: 'signup',
+      })
+    }
+    function clickFindPw() {
+      router.push({
+        name: 'findpw',
+      })
+    }
+    return {state, loginForm, clickSignUp, clickFindPw}
+  },
   data() {
     return {
-      email : '',
-      password: '',
       emailRules: [
         v => !!v || '이메일을 입력해 주세요',
         v => /.+@.+\..+/.test(v) || '이메일이 유효하지 않습니다.',
       ],
     }
   },
-  methods: {
-    clickLogIn() {
-      console.log('dd')
-    },
-    clickSignUp() {
-      this.$router.push('signup')
-    },
-    clickFindPw() {
-      this.$router.push('findpw')
-    }
-  }
 }
 </script>
 
