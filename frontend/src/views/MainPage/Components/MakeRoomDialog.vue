2c257de64a49193eb08b3feaa0054b10cb79dad4
Daniel LEE <dmsgur7112@gmail.com>
2023-02-04 19:20:32 +0900

    add : 방 입장기능 추가

--- frontend/src/views/MainPage/Components/MakeRoomDialog.vue
+++ frontend/src/views/MainPage/Components/MakeRoomDialog.vue
@@ -26,11 +26,11 @@
           <div id="join" v-if="!state.session">
             <div id="join-dialog" class="jumbotron vertical-center">
               <div class="form-group">
-                <v-text-field 
-                v-model="state.title" 
+                <v-text-field
+                v-model="state.title"
                 class="form-control"
-                label="방 제목" 
-                type="text" 
+                label="방 제목"
+                type="text"
                 required></v-text-field>
               </div>
             </div>
@@ -67,7 +67,7 @@
               value="radio-2"
             ></v-radio>
           </v-radio-group>
-          <v-text-field 
+          <v-text-field
             v-if="state.isSecret ==='radio-2'"
             label="비밀번호 숫자 4자리를 입력하세요."
             hide-details="auto"
@@ -77,7 +77,7 @@
           <v-card-actions>
             <v-spacer></v-spacer>
             <p class="text-center">
-              <v-btn 
+              <v-btn
               @click="joinSession()">Join!</v-btn>
             </p>
           </v-card-actions>
@@ -97,19 +97,19 @@
   $axios.defaults.headers.post['Content-Type'] = 'application/json';
   // const OPENVIDU_SERVER_URL = "https://localhost:4443";
   // const OPENVIDU_SERVER_SECRET = "MY_SECRET";
- 
+
   export default {
     setup() {
       const router = useRouter()
       const store = useStore()
       // const OV = computed(() => store.state.gameStore.OV)
       // const session = computed(() => store.state.gameStore.session)
-      // const mainStreamManager = computed(() => store.state.gameStore.mainStreamManager ) 
+      // const mainStreamManager = computed(() => store.state.gameStore.mainStreamManager )
       // const publisher = computed(() => store.state.gameStore.publisher )
-      // const subscribers = computed(() => store.state.gameStore.subscribers ) 
-      // const mySessionId = computed(() => store.state.gameStore.mySessionId ) 
+      // const subscribers = computed(() => store.state.gameStore.subscribers )
+      // const mySessionId = computed(() => store.state.gameStore.mySessionId )
       // const myUserName = computed(() => store.state.gameStore.myUserName )
-      
+
       const state = reactive({
         dialog: false,
         title: null,
@@ -121,14 +121,14 @@
         }
       })
       const joinSession = async function() {
-        
+
         if (state.isTeamBattle === "radio-1") {
           console.log("team 적용됨")
           state.isTeamBattle = true
         } else {
           state.isTeamBattle = false
         }
-        
+
         if (state.isSecret === "radio-1") {
           state.isSecret = false
         } else {
@@ -138,15 +138,15 @@
         console.log(state.isSecret);
         console.log(state.password);
         console.log(state.isTeamBattle);
-        await store.commit('gameStore/setTitle', state.title)
-        await store.commit('gameStore/setSecret', state.isSecret)
-        await store.commit('gameStore/setPassword', state.password)
-        await store.commit('gameStore/setTeam', state.isTeamBattle)
-        await store.dispatch('gameStore/joinSession')
-        router.push('waiting')
+        store.commit('gameStore/setTitle', state.title)
+        store.commit('gameStore/setSecret', state.isSecret)
+        store.commit('gameStore/setPassword', state.password)
+        store.commit('gameStore/setTeam', state.isTeamBattle)
+        // await store.dispatch('gameStore/joinSession')
+        router.push('gameroom')
       }
       return {
-        router, 
+        router,
         state,
         joinSession,
         // == OpenVidu State ==
