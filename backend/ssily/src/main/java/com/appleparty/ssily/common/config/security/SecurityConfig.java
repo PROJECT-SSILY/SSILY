4c827610c701a1d0d549eb253b8ac855a27170f8
ss0takk <ss0takk@gmail.com>
2023-02-07 12:10:56 +0900

    add : 그림 채점

--- backend/ssily/src/main/java/com/appleparty/ssily/common/config/security/SecurityConfig.java
+++ backend/ssily/src/main/java/com/appleparty/ssily/common/config/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) {
         web.ignoring()
-                .antMatchers("/favicon.ico", "/model.json");
+                .antMatchers("/favicon.ico", "/*.json", "/*.bin");
     }
 
     @Override
