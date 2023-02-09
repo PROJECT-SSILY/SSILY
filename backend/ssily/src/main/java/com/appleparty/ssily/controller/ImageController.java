5c88b34649585800f99142c1a256c992c361a064
ss0takk <ss0takk@gmail.com>
2023-02-09 23:14:53 +0900

    add : 정답 이미지 조회

--- backend/ssily/src/main/java/com/appleparty/ssily/controller/ImageController.java
+++ backend/ssily/src/main/java/com/appleparty/ssily/controller/ImageController.java
@@ -8,11 +8,15 @@ import com.appleparty.ssily.exception.image.ImageNotFoundException;
 import com.appleparty.ssily.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.sql.Blob;
+import java.sql.SQLException;
 import java.util.Base64;
 
 @Slf4j
@@ -65,5 +69,53 @@ public class ImageController {
         log.info("imageArray = {}", imageArray);
         imageService.saveAnswerImage(name, imageArray);
         return responseService.getSuccessResult();
+    }
+
+    @GetMapping("{name}")
+    public SingleResult<String> findImage(@PathVariable("name") String name){
+        Blob blob = imageService.findImage(name);
+        byte[] arr = blobToBytes(blob);
+
+        if(arr != null && arr.length > 0){
+            String base64Encode = byteToBase64(arr);
+            base64Encode = "data:image/png;base64," + base64Encode;
+            return responseService.getSingleResult(base64Encode);
+        }
+
+        throw new ImageNotFoundException();
+    }
+
+    // blob 데이터를 바이트로 변환
+    private byte[] blobToBytes(Blob blob){
+        BufferedInputStream br = null;
+        byte[] bytes = null;
+
+        try{
+            br = new BufferedInputStream(blob.getBinaryStream());
+            bytes = new byte[(int) blob.length()];
+            int len = bytes.length;
+            int offset = 0;
+            int read = 0;
+
+            while(offset < len &&
+                    (read = br.read(bytes, offset, len-offset)) >= 0){
+                offset += read;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bytes;
+    }
+
+    // byte를 base64로 인코딩
+    private String byteToBase64(byte[] arr){
+        String result = "";
+        try {
+            result = Base64Utils.encodeToString(arr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
