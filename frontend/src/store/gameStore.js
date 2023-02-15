c58b083bc5ec9bdbece6730f58ac636fc577018b
sooyeonlee127 <sooyeonlee127@gmail.com>
2023-02-15 14:38:18 +0900

    fix : 음소거 시점 수정

--- frontend/src/store/gameStore.js
+++ frontend/src/store/gameStore.js
@@ -283,13 +283,11 @@ const mutations = {
     },
     setInGame: (state,data) => {
       state.inGame = data
-      if (data == true) {
-        // 게임 시작했을 때 음소거
+        // 게임 시작, 종료 변동 시 음소거
         state.publisher.publishAudio(false);
         for (var j=0; state.subscribers.length > j; j++ ){
           state.subscribers[j].subscribeToAudio(false)
         }
-      }
     },
     setUserIsHost: (state, data) => {
       var index = data.index
